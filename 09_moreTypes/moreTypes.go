259d5625188ee05dce8b58d7bf82549051ad258c
adminbg <adminbg@miwl312056.mi.edeka.net>
2018-05-05 16:29:53 +0200

    Slices and Maps

--- 09_moreTypes/moreTypes.go
+++ 09_moreTypes/moreTypes.go
@@ -43,6 +43,125 @@ func slices() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	var s []int = primes[1:4] //indizes 1-3
 	fmt.Println(s)
+
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+
+	//defaults lowbound = 0 highbound = length of slice
+	s1 := []int{2, 3, 5, 7, 11, 13}
+
+	s1 = s1[1:4]
+	fmt.Println(s1)
+
+	s1 = s1[:2]
+	fmt.Println(s1)
+
+	s1 = s1[1:]
+	fmt.Println(s1)
+}
+
+func sliceLiterals() {
+	q := []int{2, 3, 5, 7, 11, 13} //baut erst das array dann den slice
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+}
+
+func sliceLengthCapacity() {
+	fmt.Println("Slice length and capacity")
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	//Slice to zero length.
+	s = s[:0]
+	printSlice(s)
+
+	//Extend lengtj
+	s = s[:4]
+	printSlice(s)
+
+	//Drop first two values
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func makeSlice() {
+	fmt.Println("make slice")
+	a := make([]int, 5) //len(a) = 5
+	printSliceS("a", a)
+
+	b := make([]int, 0, 5) //len(b) = 0 cap(b) = 5
+	printSliceS("b", b)
+
+	c := b[:2]
+	printSliceS("c", c)
+
+	d := c[2:5]
+	printSliceS("d", d)
+}
+
+func printSliceS(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
+
+//append will resize the backing array as needed
+func appendToSlice() {
+	fmt.Println("Append to Slice")
+	var s []int
+	printSlice(s)
+
+	//append works on nil slices
+	s = append(s, 0)
+	printSlice(s)
+
+	s = append(s, 1)
+	printSlice(s)
+
+	//add more than one element
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func rangeLoop() {
+	fmt.Println("Range loop")
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	//i : index
+	//v: copy of value
+	//one can skip any of these with _
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
 
 func main() {
@@ -68,4 +187,9 @@ func main() {
 	//Slices
 	fmt.Println("Calling Slices:")
 	slices()
+	sliceLiterals()
+	sliceLengthCapacity()
+	makeSlice()
+	appendToSlice()
+	rangeLoop()
 }
---
+++ 10_sliceExercise/sliceExercise.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	sl := make([][]uint8, dy)
+	for i := 0; i < len(sl); i++ {
+		sl[i] = make([]uint8, dx)
+		for j := 0; j < len(sl[i]); j++ {
+			sl[i][j] = uint8((dx + dy) / 2)
+		}
+	}
+	return sl
+}
+
+func main() {
+	pic.Show(Pic)
+}
