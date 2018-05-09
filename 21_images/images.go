6c83614e717f4b5992410e3abe8771e602d3df78
adminbg <37114412+makko86@users.noreply.github.com>
2018-05-09 13:52:30 +0200

    image exercise

---
+++ 21_images/images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type MyImage struct {
+	W, H int
+}
+
+func (m *MyImage) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (m *MyImage) Bounds() image.Rectangle {
+	return image.Rect(0, 0, m.W, m.H)
+}
+
+func (m *MyImage) At(x, y int) color.Color {
+	return color.RGBA{uint8(x) ^ uint8(y), uint8(x) ^ uint8(y), 255, 255}
+}
+
+func main() {
+	m := MyImage{50, 50}
+	pic.ShowImage(&m)
+}
